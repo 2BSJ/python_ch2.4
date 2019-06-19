a5a250b83f2bfe721ba54c31ee38f99aed360541
2BSJ <h3tmdwns@gmail.com>
2019-06-19 16:42:15 +0900

    Initial commit

---
+++ condition.py
@@ -0,0 +1,15 @@
+# if ~ else
+
+a = 10
+if a>5:
+    print('big')
+else:
+    print('small')
+
+n = -2
+if n > 0:
+    print('양수')
+elif n<0:
+    print('음수')
+else:
+    print('0')
---
+++ while.py
@@ -0,0 +1,7 @@
+count = 1
+
+while count < 11:
+    print(count, end=' ')
+    count +=1
+else:
+    print('ok')
